27951132e06886a1587fa81389e02a4eb0abe5f5
Toby Dragon <6741052+tobydragon@users.noreply.github.com>
2025-04-17 22:04:03 -0400

    Added an example test

--- test.ts
+++ test.ts
@@ -1,4 +1,13 @@
-// Add your code here
-function testPathStep():void{
-    
+// just an eample of how you can fail a test, and how you can write output
+// to see a failing test, change the value of x
+function exampleTest():void{
+    let x = 5;
+    if (x===5){
+        console.log("Example Test is all good");
+    }
+    else {
+        throw "Testing how to show an error";
+    }
 }
+
+exampleTest();
